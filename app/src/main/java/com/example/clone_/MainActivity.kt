903c4fefe0b2ec586195fae0b4e4041bdbf34bf4
ha-es <dmstj00423@naver.com>
2023-11-04 14:32:51 +0900

    1104 -u (가수이름 error 해결)

--- app/src/main/java/com/example/clone_/MainActivity.kt
+++ app/src/main/java/com/example/clone_/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         initBottomNavigation()
 
 
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerTitleTv.text.toString(),0,60,false)
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(),0,60,false)
 
 
         binding.mainPlayerCl.setOnClickListener {
